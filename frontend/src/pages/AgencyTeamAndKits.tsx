949ba95d6d3ece7c621b7409391a33395db14258
phemmy1080 <107289845+phemmy1080@users.noreply.github.com>
2026-05-17 05:29:20 +0100

    Update AgencyTeamAndKits.tsx

--- frontend/src/pages/AgencyTeamAndKits.tsx
+++ frontend/src/pages/AgencyTeamAndKits.tsx
@@ -1,7 +1,5 @@
 import { useState, useEffect } from "react";
-import { useNavigate } from "react-router-dom";
-//import { api } from "./api";
-import { api } from "../lib/api";
+import { api } from '../lib/api';
 
 // ── Types ─────────────────────────────────────────────────────────────────────
 interface Member {
