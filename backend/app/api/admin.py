654343748bbb42076bceb0feaeba90fd5e686051
phemmy1080 <107289845+phemmy1080@users.noreply.github.com>
2026-05-29 21:25:28 +0100

    Update admin.py

--- backend/app/api/admin.py
+++ backend/app/api/admin.py
@@ -15,7 +15,7 @@ import uuid
 from datetime import datetime, timezone
 from typing import Optional
 
-from fastapi import APIRouter, HTTPException, Header, Depends, Request
+from fastapi import APIRouter, HTTPException, Header, Depends, Request, BackgroundTasks
 from fastapi.responses import StreamingResponse
 from pydantic import BaseModel
 
